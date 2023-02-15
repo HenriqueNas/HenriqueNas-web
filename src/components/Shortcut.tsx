dbfe869eb27c037f399367240d5a77d40b3a3b22
HenriqueNas <brkshen@gmail.com>
2023-02-15 09:39:01 -0300

    feat: :sparkles: add Shortcut component

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -1,23 +1,30 @@
 'use client'
 
+import Image, { StaticImageData } from 'next/image'
+
 interface ShortcutProps {
-  icon: string
+  image: StaticImageData
   title: string
   onClick?: () => void
 }
 
-export default function Shortcut({ icon, title, onClick }: ShortcutProps) {
+export default function Shortcut({ image, title, onClick }: ShortcutProps) {
   return (
     <div
-      onClick={onClick}
-      className="w-20 h-20 flex flex-col items-center justify-center m-2 bg-transparent cursor-pointer"
+      onDoubleClick={onClick}
+      className="
+        w-20 h-20 m-2 
+        flex flex-col items-center justify-center 
+        bg-transparent cursor-pointer 
+        hover:filter hover:brightness-50
+      "
     >
+      <Image src={image} alt={`${title} icon`} width={40} height={40} />
       <span
         className="
         flex items-center justify-center 
-        px-[5px] py-[2.5px] bg-[#FFF]/40 
-        text-[#262626] text-lg
-        hover:bg-[#262626] hover:text-[#FFF]
+        mt-2 px-2 bg-[#FFF]/40 
+        text-black text-xl
         "
       >
         {title}
